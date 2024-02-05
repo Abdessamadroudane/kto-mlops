fb76924f0b4d26f9d8e631847db83d404fd67930
Abdessamad ROUDANE <83363805+Abdessamadroudane@users.noreply.github.com>
2024-02-05 14:05:12 +0000

    feat : Mon premier script python

--- mon_premier_script.py
+++ mon_premier_script.py
@@ -1,15 +1,29 @@
-"""
-Count names with more than seven letters
-"""
-def names(prenoms):
-    more_than_seven = 0
-    for prenom in prenoms:
-        if len(prenom) > 7:
-            more_than_seven += 1
-            print("Prenom supérieur à 7 : " + prenom)
-        else:
-            print("Prenom inférieur ou égal à 7 : " + prenom)
-    return more_than_seven
+import unittest
 
-prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
-print("Nombre de prénoms supérieurs à 7 : " + str(names(prenoms=prenoms)))
+def count_names_with_more_than_seven_letters(noms):
+    """
+    Compte les noms de plus de sept lettres.
+
+    Args:
+        noms (list of str): Une liste de noms.
+
+    Returns:
+        int: Le nombre de noms de plus de sept lettres.
+    """
+    compte = 0
+    for nom in noms:
+        if len(nom) > 7:
+            compte += 1
+            print("Nom avec plus de sept lettres:", nom)
+        else:
+            print("Nom avec sept lettres ou moins:", nom)
+    return compte
+
+class TestNamesMethod(unittest.TestCase):
+     def test_names(self):
+        noms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
+        plus_de_sept = count_names_with_more_than_seven_letters(noms)
+        self.assertEqual(plus_de_sept, 4)
+
+if __name__ == '__main__':
+    unittest.main()
